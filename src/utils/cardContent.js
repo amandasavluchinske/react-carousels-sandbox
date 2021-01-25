57db466b5c5ae5b44c1e54b0c8caa665b6dd07ac
Amanda Savluchinske <amandasavluchinske@gmail.com>
2021-01-25 16:37:44 -0300

    Adds simple cards list

---
+++ src/utils/cardContent.js
@@ -0,0 +1,55 @@
+const CARD_DATA = [
+  {
+    name: "Card 1",
+  },
+  {
+    name: "Card 2",
+  },
+  {
+    name: "Card 3",
+  },
+  {
+    name: "Card 4",
+  },
+  {
+    name: "Card 5",
+  },
+  {
+    name: "Card 6",
+  },
+  {
+    name: "Card 7",
+  },
+  {
+    name: "Card 8",
+  },
+  {
+    name: "Card 9",
+  },
+  // {
+  //   name: "Card 10",
+  // },
+  // {
+  //   name: "Card 11",
+  // },
+  // {
+  //   name: "Card 12",
+  // },
+  // {
+  //   name: "Card 13",
+  // },
+  // {
+  //   name: "Card 14",
+  // },
+  // {
+  //   name: "Card 15",
+  // },
+  // {
+  //   name: "Card 16",
+  // },
+  // {
+  //   name: "Card 17",
+  // },
+];
+
+export default CARD_DATA;
