f1925ebabe74c4a1b74e4a82a0980ac154a91c23
Amanda Savluchinske <amandasavluchinske@gmail.com>
2021-01-26 11:19:41 -0300

    Applies conditional styling to card

--- src/Card.js
+++ src/Card.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardContainer = styled.div`
-  width: 150px;
+  width: 120px;
   height: 400px;
   border: 1px solid black;
   margin: 10px;
+  ${({ fade }) => fade && `
+    background-color: red;
+  `};
 `;
 
 
-const Card = ({ name }) => {
-  return (<CardContainer>{name}</CardContainer>);
+const Card = ({ name, fade }) => {
+  return (<CardContainer fade={fade}>{name}</CardContainer>);
 };
 
 export default Card;
